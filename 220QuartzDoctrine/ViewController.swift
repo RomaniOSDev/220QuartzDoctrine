599be291c2c0c451643ba58a193ae6f77fd3511f
Roman <romazaniosdev@gmail.com>
2026-06-19 15:15:11 +0300

    Initial Commit

---
+++ 220QuartzDoctrine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  220QuartzDoctrine
+//
+//  Created by Roman on 6/19/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
